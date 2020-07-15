70d958282615548438aa4e4b5259f3850a1e22e3
TobeManager <2309839936@qq.com>
2020-07-15 17:10:11 +0800

    xxxx

--- my-rest/my-user/src/main/java/com/my/user/controller/UserController.java
+++ my-rest/my-user/src/main/java/com/my/user/controller/UserController.java
@@ -3,7 +3,9 @@ package com.my.user.controller;
 import com.my.common.ResData;
 import com.my.user.entity.User;
 import com.my.utils.ResDataUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
